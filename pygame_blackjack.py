021abb417f7a766f775240020e2cf6af83ba4c3c
Matthias Lannoo <matthias.lannoo@student.ehb.be>
2024-10-15 15:14:10 +0200

    cards check

--- pygame_blackjack.py
+++ pygame_blackjack.py
@@ -4,5 +4,5 @@ import copy
 
 cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 cards_deck = 4 * cards
-for _ in range(10):
+for x in range(10):
     print(random.choice(cards), end=" ")
