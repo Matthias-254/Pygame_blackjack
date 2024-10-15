b8fdb9257313f2b608015b7ee0891152a32f070e
Matthias-254 <lannoo.matthias@hotmail.com>
2024-10-15 15:19:25 +0200

    test 2 added

--- pygame_blackjack.py
+++ pygame_blackjack.py
@@ -6,4 +6,5 @@ cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 cards_deck = 4 * cards
 for x in range(10):
     print(random.choice(cards), end=" ")
-    print("test")
+print("\ntest")
+print("test 2")
