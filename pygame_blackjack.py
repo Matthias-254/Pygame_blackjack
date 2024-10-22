99be5f56bb56ca3e1c14ae75ceed8810581e544b
Matthias Lannoo <matthias.lannoo@student.ehb.be>
2024-10-22 19:29:33 +0200

    tekst verbeteringen

--- pygame_blackjack.py
+++ pygame_blackjack.py
@@ -5,10 +5,10 @@ cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 one_deck = 4 * cards
 decks = 4
 game_deck = copy.deepcopy(decks * one_deck)
-WIDTH = 600
-HEIGHT = 900
+WIDTH = 1280
+HEIGHT = 720
 BG = pygame.image.load("assets/Background.png")
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pygame Blackjack with Menu")
 timer = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 44)
@@ -27,7 +27,7 @@ add_score = False
 results = ['', 'PLAYER BUSTED o_O', 'Player WINS! :)', 'DEALER WINS :(', 'PUSH...']
 
 def get_font(size):
-    return pygame.font.Font("assets/font.ttf", size)
+	return pygame.font.Font("assets/font.ttf", size)
 
 class Button():
 	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
@@ -60,67 +60,67 @@ class Button():
 			self.text = self.font.render(self.text_input, True, self.base_color)
 
 def main_menu():
-    while True:
-        screen.blit(BG, (0, 0))
+	while True:
+		screen.blit(BG, (0, 0))
 
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
+		MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+		MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+		MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
-                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
-                            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
-                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+		PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+							text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+		OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400),
+							text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+		QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+							text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
-        screen.blit(MENU_TEXT, MENU_RECT)
+		screen.blit(MENU_TEXT, MENU_RECT)
 
-        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
-            button.update(screen)
+		for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+			button.changeColor(MENU_MOUSE_POS)
+			button.update(screen)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    blackjack_game()  # Roep je blackjack_spel functie op hier
-                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    options()
-                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    pygame.quit()
-                    sys.exit()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+					blackjack_game()
+				if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+					options()
+				if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+					pygame.quit()
+					sys.exit()
 
-        pygame.display.update()
+		pygame.display.update()
 
 def options():
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+	while True:
+		OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        screen.fill("white")
+		screen.fill("white")
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+		OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+		OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
+		screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(640, 460),
-                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+		OPTIONS_BACK = Button(image=None, pos=(640, 460),
+							text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
 
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(screen)
+		OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+		OPTIONS_BACK.update(screen)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+					main_menu()
 
-        pygame.display.update()
+		pygame.display.update()
 
 def deal_cards(current_hand, current_deck):
 	card = random.randint(0, len(current_deck) - 1)
@@ -161,7 +161,6 @@ def calculate_score(hand):
 			hand_score += 11
 		else:
 			hand_score += int(card)
-	# Adjust Aces as needed
 	while hand_score > 21 and aces_count > 0:
 		hand_score -= 10
 		aces_count -= 1
@@ -220,78 +219,75 @@ def check_endgame(hand_act, deal_score, play_score, result, totals, add):
 
 
 def blackjack_game():
-    global player_score, dealer_score, my_hand, dealer_hand, initial_deal, reveal_dealer, active, hand_active, add_score, outcome, records, game_deck
+	global player_score, dealer_score, my_hand, dealer_hand, initial_deal, reveal_dealer, active, hand_active, add_score, outcome, records, game_deck
 
-    WIDTH = 600
-    HEIGHT = 900
-    screen = pygame.display.set_mode([WIDTH, HEIGHT])
-    pygame.display.set_caption('Pygame Blackjack!')
+	screen = pygame.display.set_mode([WIDTH, HEIGHT])
+	pygame.display.set_caption('Pygame Blackjack!')
 
-    timer = pygame.time.Clock()
+	timer = pygame.time.Clock()
 
-    run = True
-    while run:
-        timer.tick(60)
-        screen.fill('black')
+	run = True
+	while run:
+		timer.tick(60)
+		screen.fill('black')
 
-        if initial_deal:
-            for i in range(2):
-                my_hand, game_deck = deal_cards(my_hand, game_deck)
-                dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
-            initial_deal = False
+		if initial_deal:
+			for i in range(2):
+				my_hand, game_deck = deal_cards(my_hand, game_deck)
+				dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
+			initial_deal = False
 
-        if active:
-            player_score = calculate_score(my_hand)
-            draw_cards(my_hand, dealer_hand, reveal_dealer)
-            if reveal_dealer:
-                dealer_score = calculate_score(dealer_hand)
-                if dealer_score < 17:
-                    dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
-            draw_scores(player_score, dealer_score)
+		if active:
+			player_score = calculate_score(my_hand)
+			draw_cards(my_hand, dealer_hand, reveal_dealer)
+			if reveal_dealer:
+				dealer_score = calculate_score(dealer_hand)
+				if dealer_score < 17:
+					dealer_hand, game_deck = deal_cards(dealer_hand, game_deck)
+			draw_scores(player_score, dealer_score)
 
-        buttons = draw_game(active, records, outcome)
+		buttons = draw_game(active, records, outcome)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.MOUSEBUTTONUP:
-                if not active:
-                    if buttons[0].collidepoint(event.pos):
-                        active = True
-                        initial_deal = True
-                        my_hand = []
-                        dealer_hand = []
-                        outcome = 0
-                        hand_active = True
-                        reveal_dealer = False
-                        add_score = True
-                else:
-                    if buttons[0].collidepoint(event.pos) and player_score < 21 and hand_active:
-                        my_hand, game_deck = deal_cards(my_hand, game_deck)
-                    elif buttons[1].collidepoint(event.pos) and not reveal_dealer:
-                        reveal_dealer = True
-                        hand_active = False
-                    elif len(buttons) == 3:
-                        if buttons[2].collidepoint(event.pos):
-                            active = True
-                            initial_deal = True
-                            my_hand = []
-                            dealer_hand = []
-                            outcome = 0
-                            hand_active = True
-                            reveal_dealer = False
-                            add_score = True
-                            dealer_score = 0
-                            player_score = 0
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				if not active:
+					if buttons[0].collidepoint(event.pos):
+						active = True
+						initial_deal = True
+						my_hand = []
+						dealer_hand = []
+						outcome = 0
+						hand_active = True
+						reveal_dealer = False
+						add_score = True
+				else:
+					if buttons[0].collidepoint(event.pos) and player_score < 21 and hand_active:
+						my_hand, game_deck = deal_cards(my_hand, game_deck)
+					elif buttons[1].collidepoint(event.pos) and not reveal_dealer:
+						reveal_dealer = True
+						hand_active = False
+					elif len(buttons) == 3:
+						if buttons[2].collidepoint(event.pos):
+							active = True
+							initial_deal = True
+							my_hand = []
+							dealer_hand = []
+							outcome = 0
+							hand_active = True
+							reveal_dealer = False
+							add_score = True
+							dealer_score = 0
+							player_score = 0
 
-        if hand_active and player_score >= 21:
-            hand_active = False
-            reveal_dealer = True
+		if hand_active and player_score >= 21:
+			hand_active = False
+			reveal_dealer = True
 
-        outcome, records, add_score = check_endgame(hand_active, dealer_score, player_score, outcome, records, add_score)
+		outcome, records, add_score = check_endgame(hand_active, dealer_score, player_score, outcome, records, add_score)
 
-        pygame.display.flip()
-    pygame.quit()
+		pygame.display.flip()
+	pygame.quit()
 
-# Start het menu
 main_menu()
